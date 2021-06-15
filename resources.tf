1ae7798cac5842739396bd43b896e24a8868c373
Prashant Shetage <shetageprashant@gmail.com>
2021-06-15 16:11:57 +0530

    Initial commit

---
+++ resources.tf
@@ -0,0 +1,27 @@
+resource "azurerm_app_service_plan" "plan" {
+  name = substr(local.app_service_plan_name, 0, length(local.app_service_plan_name) > 60 ? 59 : -1)
+
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  kind                = var.kind
+  reserved            = var.kind == "Linux" ? true : var.reserved
+
+  sku {
+    capacity = lookup(var.sku, "capacity", local.default_sku_capacity)
+    size     = lookup(var.sku, "size", null)
+    tier     = lookup(var.sku, "tier", null)
+  }
+
+  tags = merge(local.default_tags, var.extra_tags)
+}
+
+module "diagnostics" {
+  source  = "claranet/diagnostic-settings/azurerm"
+  version = "4.0.1"
+
+  resource_id           = azurerm_app_service_plan.plan.id
+  logs_destinations_ids = var.logs_destinations_ids
+  log_categories        = var.logs_categories
+  metric_categories     = var.logs_metrics_categories
+  retention_days        = var.logs_retention_days
+}
---
+++ variables.tf
@@ -0,0 +1,86 @@
+variable "location" {
+  description = "Azure location."
+  type        = string
+}
+
+variable "location_short" {
+  description = "Short string for Azure location."
+  type        = string
+}
+
+variable "client_name" {
+  description = "Client name/account used in naming"
+  type        = string
+}
+
+variable "environment" {
+  description = "Project environment"
+  type        = string
+}
+
+variable "stack" {
+  description = "Project stack name"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "name_prefix" {
+  description = "Optional prefix for the generated name"
+  type        = string
+  default     = ""
+}
+
+variable "sku" {
+  description = "A sku block. See documentation https://www.terraform.io/docs/providers/azurerm/r/app_service_plan.html#sku"
+  type        = map(string)
+}
+
+variable "kind" {
+  description = "The kind of the App Service Plan to create. See documentation https://www.terraform.io/docs/providers/azurerm/r/app_service_plan.html#kind"
+  type        = string
+}
+
+variable "extra_tags" {
+  description = "Extra tags to add"
+  type        = map(string)
+  default     = {}
+}
+
+variable "custom_name" {
+  description = "Name of the App Service Plan, generated if not set."
+  type        = string
+  default     = ""
+}
+
+variable "reserved" {
+  description = "Flag indicating if App Service Plan should be reserved. Forced to true if \"kind\" is \"Linux\"."
+  type        = string
+  default     = "false"
+}
+
+variable "logs_destinations_ids" {
+  type        = list(string)
+  description = "List of destination resources Ids for logs diagnostics destination. Can be Storage Account, Log Analytics Workspace and Event Hub. No more than one of each can be set. Empty list to disable logging."
+}
+
+variable "logs_categories" {
+  type        = list(string)
+  description = "Log categories to send to destinations."
+  default     = null
+}
+
+variable "logs_metrics_categories" {
+  type        = list(string)
+  description = "Metrics categories to send to destinations."
+  default     = null
+}
+
+variable "logs_retention_days" {
+  type        = number
+  description = "Number of days to keep logs on storage account"
+  default     = 30
+}
